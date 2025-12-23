0a2795ab73b6ee06755b16a2bd0b29c6f2fe4a99
foozbat <github@foozbat.net>
2025-12-23 09:59:56 -0600

    Update schema

--- AzureSQLDbContext.cs
+++ AzureSQLDbContext.cs
@@ -15,9 +15,11 @@ public class AzureSQLDbContext : DbContext
         modelBuilder.Entity<Lead>(entity =>
         {
             entity.HasKey(e => e.Id);
-            entity.Property(e => e.Name).IsRequired().HasMaxLength(200);
+            entity.Property(e => e.FirstName).IsRequired().HasMaxLength(200);
             entity.Property(e => e.Email).IsRequired().HasMaxLength(200);
             entity.Property(e => e.Phone).IsRequired().HasMaxLength(50);
+            entity.Property(e => e.CorrelationId).HasMaxLength(100);
+            entity.HasIndex(e => e.CorrelationId);
             entity.Property(e => e.CreatedAt).IsRequired();
             entity.Property(e => e.UpdatedAt).IsRequired();
         });
--- Lead.cs
+++ Lead.cs
@@ -1,9 +1,11 @@
 public class Lead
 {
     public int Id { get; set; }
-    public required string Name { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
     public required string Email { get; set; }
     public required string Phone { get; set; }
+    public string? CorrelationId { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 }
